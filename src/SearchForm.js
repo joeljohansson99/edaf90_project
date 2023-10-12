70376847df1a7c48c6be8183e165f6a5b407f759
Joel Johansson <joel.k.johansson@gmail.com>
2023-10-12 13:33:13 +0200

    Implemented pagination
    However, now next does not work if you blanked the search fields and click next. Maybe we need to save old values of the fields? And only update them during a new search

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -6,15 +6,18 @@ import BookModal from "./BookModal"
 import Image from 'react-bootstrap/Image';
 import axios from 'axios';
 import { useOutletContext, useLoaderData } from "react-router-dom";
+import Pagination from 'react-bootstrap/Pagination';
 
 
 function SearchForm() {
-
+    const MAX_BOOKS = 10;
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
     const [isbn, setIsbn] = useState("");
 
     const [books, setBooks] = useState([])
+    const [totalBooks, setTotalBooks] = useState(0)
+    const [currentPage, setCurrentPage] = useState(0)
 
     const {cart, setCart} = useOutletContext();
     const {fav, setFav} = useOutletContext();
@@ -31,6 +34,16 @@ function SearchForm() {
 
     function onIsbnChange(e) {
         setIsbn(e.target.value)
+    }
+
+    function incPage(e) {
+        setCurrentPage(currentPage+1);
+        search(currentPage+1);
+    }
+
+    function decPage(e) {
+        setCurrentPage(currentPage-1);
+        search(currentPage-1);
     }
 
     function handleBookClick(e) {
@@ -52,12 +65,17 @@ function SearchForm() {
 
     function onSearch(e) {
         e.preventDefault();
+        setCurrentPage(0);
+        search(0);
+    }
 
+    function search(page) {
         const titleParam = title !== "" ? `intitle:${title}` : "";
         const authorParam = author !== "" ? `inauthor:${author}` : "";
         const isbnParam = isbn !== "" ? `isbn:${isbn}` : "";
+        const indexParam = `startIndex=${page*MAX_BOOKS}&MaxResults=${MAX_BOOKS}`;
 
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${titleParam}+${authorParam}+${isbnParam}`;
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${titleParam}+${authorParam}+${isbnParam}&${indexParam}`;
 
         console.log(`GET to ${url}`);
 
@@ -67,8 +85,13 @@ function SearchForm() {
             }
             return response.data;
         }).then(data => {
-            setBooks(data.items);
-        })
+            console.log(data)
+            setBooks(data.items ? data.items : []);
+            setTotalBooks(data.totalItems);
+        }).catch(error => {
+            console.log('Error!')
+            setBooks([]);
+        });
     }
     
     return (
@@ -93,14 +116,21 @@ function SearchForm() {
                     Search
                 </Button>
             </Form>
-            <ButtonGroup vertical>
+            <ButtonGroup vertical className='mb-5'>
                 {books.length > 0 ? 
-                    books.map(book => <div key={book.id} >
-                            <Image src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "ingenBild.png"}  thumbnail/>
-                            <Button variant="link" className='border' name={book.id} onClick={handleBookClick}>
-                                {book.volumeInfo.title} - {book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "No Author"}
-                            </Button>
-                        </div>)
+                    <>
+                        <div className="p-2 m-2 text-muted">Showing {currentPage*MAX_BOOKS+1} - {Math.min((currentPage+1)*MAX_BOOKS, totalBooks)} out of {totalBooks} books</div>
+                        {books.map(book => <div key={book.id} className='d-flex align-items-center' >
+                                <Image src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "ingenBild.png"}  thumbnail/>
+                                <Button variant="link" className='border' name={book.id} onClick={handleBookClick}>
+                                    {book.volumeInfo.title} - {book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "No Author"}
+                                </Button>
+                            </div>)}
+                        <Pagination className='mt-3'>
+                            <Pagination.Item onClick={decPage} disabled={currentPage === 0} >Prev</Pagination.Item>
+                            <Pagination.Item onClick={incPage} disabled={(currentPage+1) * MAX_BOOKS >= totalBooks}>Next</Pagination.Item>
+                        </Pagination>
+                    </>
                     : 
                     <div className="pt-3 mt-4 text-muted border-top">No books found</div>
                 }
