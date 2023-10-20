568b3dbe1fb7c2f02c7d0f70268eb7fb82a188bb
joel.johansson.5758 <joel.k.johansson@gmail.com>
2023-10-20 17:32:47 +0200

    fix unused imports

--- src/Toast.js
+++ src/Toast.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Toast from 'react-bootstrap/Toast';
